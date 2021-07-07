8a2ef22ec866e823777b990ef8ec010c7671e9e6
Glenn Etchells <glenn.etchells@outlook.com>
2021-07-07 18:03:07 +0100

    added helloworld

---
+++ src/com/me/helloworld/Runner.java
@@ -0,0 +1,52 @@
+package com.me.helloworld;
+
+public class Runner {
+
+	public static void printMessage(String message) {
+		System.out.println(message);
+		
+	}
+
+	public static void main(String[] args) {
+		printMessage("Hello World!");
+	}
+	}	
+		
+
+
+
+
+	//boolean bool = true;
+	//byte bytes = 8;
+	//char character = 'A';
+	//short number = 16;
+	//int anotherNumber = 32;
+	//long aLongNumber = 64L;
+	//float decimalNumber = 3.2f;
+	//double anotherDecimalNumber = 6.4d;
+
+	//String myText = "Hello World!";
+			
+
+
+	//public int methodName() {
+//	    return 0;
+	//}
+	//public boolean methodName() {
+//	    return true;
+	//}
+	//public char methodName() {
+//	    return 'P';
+	//}
+	//public long methodName() {
+//	    return 52L;
+	//}
+	//public float methodName() {
+//	    return 0.6f;
+	//}
+	//public String methodName() {
+//	    return "Hi there";
+	//}
+	//public void methodName() {
+	    // no return
+	//}
